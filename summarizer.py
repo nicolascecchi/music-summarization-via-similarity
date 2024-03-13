94f68cdd5488e54f73fb7aa3d7b656ea474dc3ea
Nicolas Cecchi <mail>
2024-03-13 16:06:26 +0100

    Added save summaries. Working on plot score with highlight

--- summarizer.py
+++ summarizer.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # directly in the web browser
 from IPython.display import Audio
 from matplotlib import patches
-
+from scipy.io import wavfile
 
 
 def moving_average(x, w):
@@ -19,13 +19,14 @@ def moving_average(x, w):
 class SongSummary():
     
     def __init__(self,path, sr = 22050):
-        if type(path)=='str':
+        try:
             self.raw_song, self.sr = librosa.load(path)  
-        elif type(path) == np.ndarray:
-            self.raw_song = path
-            self.sr = sr
-        else: 
-            raise('Path is either string to file audio or np.ndarray.')
+        except:
+            if type(path) == np.ndarray:
+                self.raw_song = path
+                self.sr = sr
+            else: 
+                raise('Path is either string to file audio or np.ndarray.')
         self.song, self.trim_idx  = librosa.effects.trim(self.raw_song)
         self.sim_matrix = None
         
@@ -36,7 +37,7 @@ class SongSummary():
         self.win_seconds = self.win_size / self.sr # window size in seconds
         self.effective_win_size = int(self.win_size - self.win_size/8) # Hardcoded because of overlap hyperparam
         self.win_step_seconds = self.effective_win_size / self.sr # size of one step, consider overlap
-        self.summary_size= summary_time * self.sr // self.effective_win_size#self.win_size # in windows
+        self.summary_size = summary_time * self.sr // self.win_size # in nb of effective windows
 
         # Define which side of the argsort will be selected as a (dis)similar
         if is_similarity:
@@ -68,7 +69,10 @@ class SongSummary():
 
     
     def _get_ticks_and_labels(self, extend=False):
-        
+        '''
+        Ticks are returned in Windows.
+        Labels in seconds.
+        '''
         if not extend:
             duration = len(self.sim_matrix)+1
         else:
@@ -82,14 +86,16 @@ class SongSummary():
         labels.extend(list(int(self.window_to_second(_)) for _ in ticks[1:]))
         return ticks, labels
     
-    def plot_matrix(self, highlight=False, triple=False):
+    def plot_matrix(self, highlight=False, triple=False, save_as=False):
         f, ax = plt.subplots()
         img = ax.imshow(self.sim_matrix)
         ticks, labels = self._get_ticks_and_labels()
-        
+
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('time (s)')
         ax.set_yticks(ticks,labels)
         ax.set_xticks(ticks,labels)
-        
+        ax.set_aspect('equal', 'box')
         plt.colorbar(img)
         if highlight:
             if not triple:
@@ -98,14 +104,19 @@ class SongSummary():
                 h = self.sim_matrix.shape[0] * self.summary_size/(len(self.song)/self.win_size)
                 rect = patches.Rectangle((0, self.best-2), self.sim_matrix.shape[0],h+2, linewidth=1, edgecolor='r', facecolor='none')
                 ax.add_patch(rect)
-        plt.show()
-        
+        if save_as==False:
+            plt.show()
+        else:
+            plt.savefig('./imgs/{}.png'.format(save_as), bbox_inches='tight',format='png')
+
+    
     def plot_song(self, highlight=False, triple=False):
         f, ax = plt.subplots()
         songplot = ax.plot(self.song)
-        if self.sim_matrix:
+        if self.sim_matrix is not None:
             ticks, labels = self._get_ticks_and_labels()
-            ax.set_xticks(ticks,labels)
+            new_ticks = [self.window_to_point(_) for _ in ticks]
+            ax.set_xticks(new_ticks,labels)
         
         if highlight:
             ax = plt.gca()
@@ -119,7 +130,7 @@ class SongSummary():
 
         plt.show()
 
-    def plot_scores(self, extend=False, highlight=False):
+    def plot_scores(self, extend=False, highlight=False, save_as=False, triple=False):
         
         f, ax = plt.subplots()
         
@@ -136,9 +147,16 @@ class SongSummary():
         
         # Highlight summary section
         if highlight:
-            raise('NotImplementedError')
-               
-        plt.show()
+            ax = plt.gca()
+            if not triple:
+                # Create a Rectangle patch
+                ax.axvspan(self.point_to_window(self.start),self.point_to_window(self.end) , alpha=0.25, color='green', )
+        #if highlight:
+        #    raise NotImplementedError('Feature not implemented yet!')
+        if save_as==False:
+            plt.show()
+        else:
+            plt.savefig('./imgs/{}.png'.format(save_as), bbox_inches='tight',format='png')
 
     def compute_scores(self, sliding_size, similarities):
         sim_matrix_j = np.mean(similarities,axis=1)
@@ -149,8 +167,8 @@ class SongSummary():
         
     def summarize_cooperfoote(self): 
         self.compute_scores(self.summary_size, self.sim_matrix)
-        self.best = self.sorted[self.similar]
-        self.start = self.best*int(self.win_size-self.win_size/8) # accounts for overlapping
+        self.best = self.sorted[self.similar] # Gets the first or last index
+        self.start = self.best * self.effective_win_size # accounts for overlapping
         self.end = self.start+self.summary_size*self.win_size
         self.summary.update({'cooper-foote':self.song[self.start:self.end]})
 
@@ -185,15 +203,27 @@ class SongSummary():
     def play_summary(self, type):
         '''Type one of <cooper-foote> or <triple>.'''
         return Audio(self.summary[type],rate=self.sr)
-
+        
+    def save_summary(self, path, type):
+        try:
+            wavfile.write(path+'.wav', rate=self.sr, data=self.summary[type])
+        except:
+            raise('Something wrong happened')
+            
     def point_to_second(self, point):
         return point / self.sr 
+        
+    def point_to_window(self, window):
+        return self.second_to_point(self.window_to_second(window))
         
     def second_to_point(self, time):
         return time * self.sr
 
     def second_to_window(self, time):
         return time * self.sr / self.effective_win_size 
-        
+    
+    def window_to_point(self, window):
+        return self.second_to_point(self.window_to_second(window))    
+    
     def window_to_second(self, window):
         return window * self.effective_win_size / self.sr 
